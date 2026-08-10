4969a742eb5e277f38c709455945e799ffce6514
valfresantiago-wq <valfresantiago@gmail.com>
2026-08-10 12:24:56 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "Mi primera API funciona"}
+
+
+@app.get("/productos")
+def productos():
+    return [
+        {"id": 1, "nombre": "Resma A4", "precio": 6000},
+        {"id": 2, "nombre": "Lapicera", "precio": 879}
+    ]
