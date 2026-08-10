200d3e3cff8c1106429587691f34a39768871d42
valfresantiago-wq <valfresantiago@gmail.com>
2026-08-10 17:17:45 -0300

    Update main.py
    Agregar CORS

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://valfresantiago-wq.github.io"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def inicio():
     return {"mensaje": "Mi primera API funciona"}
 
-
 @app.get("/productos")
 def productos():
     return [
         {"id": 1, "nombre": "Resma A4", "precio": 6000},
-        {"id": 2, "nombre": "Lapicera", "precio": 879}
+        {"id": 2, "nombre": "Lapicera", "precio": 999,99}
     ]
